72aabc5c0d64f9c55c214bc4c17bc48732e9b73e
reprostryyl <104348852+reprostryyl@users.noreply.github.com>
2023-02-16 16:20:09 +0300

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -5,7 +5,6 @@ def assign_learning_rate(optimizer, new_lr):
     for param_group in optimizer.param_groups:
 
 def _warmup_lr(base_lr, warmup_length, step):
-    return base_lr * (step + 1) / warmup_length
 
 
 def cosine_lr(optimizer, base_lr, warmup_length, steps):
